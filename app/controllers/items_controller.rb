813476d620981c01794a02a1166d94d6aca64cf8
keveeen <105822487+keveeen@users.noreply.github.com>
2023-01-28 15:43:26 +0100

    commit tree d3dd7b8a6129f7bb065c00a8a630f0dbeab91c92

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,50 @@
+
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :was_not_found
+
 
   def index
+
+    if params[:user_id]
+       
+      user = User.find(params[:user_id])
+
+      items = user.items
+
+    else
     items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def show
+
+    item = Item.find(params[:id])
+    render json: item
+
+  end
+
+  def create
+
+    user = User.find_by(params[:user_id])
+    item = Item.create(item_params)
+    create = user.items << item
+
+    render json: item, status: :created
+    
+
+  end
+  
+  private
+  
+  def was_not_found
+  
+    render json: {error: "Not found..."}, status: :not_found
+  end
+
+
+  def item_params
+    params.permit(:name, :description, :price)
   end
 
 end
